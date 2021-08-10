5e7a2f07c263a31d3fd36b6916a2711d53041dc7
germanrk <german.rk@hotmail.com>
2021-08-10 16:24:21 -0300

    Primera prueba de login con google

--- js/login.js
+++ js/login.js
@@ -2,5 +2,11 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
 });
