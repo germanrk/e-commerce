32ceb6208e85f2c8e42256781c91cb843e6235c2
germanrk <german.rk@hotmail.com>
2021-08-10 18:13:32 -0300

    prueba 1

--- js/login.js
+++ js/login.js
@@ -1,12 +1,12 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      }
-});
+let idUser = ``;
+let nameUser = ``;
+let imgUser = ``;
+let emailUser = ``;
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    idUser = profile.getId();
+    nameUser = profile.getName();
+    imgUser = profile.getImageUrl();
+    emailUser = profile.getEmail();
+}
