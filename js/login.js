7056ae4ba546a4c16dc501a64cc7349aa31b5ea2
germanrk <german.rk@hotmail.com>
2021-08-10 20:15:27 -0300

    Prueba

--- js/login.js
+++ js/login.js
@@ -1,14 +1,29 @@
 let idUser = ``;
-let nameUser = ``;
+let firstNameUser = ``;
+let lastNameUser = ``;
 let imgUser = ``;
 let emailUser = ``;
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    var idTokenUser = googleUser.getAuthResponse().id_token;
     idUser = profile.getId();
-    nameUser = profile.getName();
+    firstNameUser = profile.getFirstName();
+    lastNameUser = profile.getLasName();
     imgUser = profile.getImageUrl();
     emailUser = profile.getEmail();
-    console.log(googleUser)
-    console.log(profile)
+    console.log(googleUser);
+    console.log(profile);
+    console.log(idTokenUser);
+    console.log(firstNameUser)
+    console.log(idUser)
+    console.log(lastNameUser)
+
+    if(idUser){
+        let htmlContentToAppend = "";
+        htmlContentToAppend += `
+            <div><a class="btn btn-denger" href="./inicio.html">Entrar</a></div>
+        `
+        document.getElementById("intro").innerHTML = htmlContentToAppend;
+    }
 }
